ef057e452a565335b025b2d943e0a0d7261d64a8
prabhat-cn <prabhat@capitalnumbers.com>
2023-08-26 13:03:57 +0530

    react installed with component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MainTodo from './pages/todoApp/MainTodo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React18-Applications</h1>
+      <MainTodo />
     </div>
   );
 }
---
+++ src/pages/todoApp/MainTodo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainTodo = () => {
+  return (
+    <>
+      <div>gjhghj</div>
+      <div>gjhghj</div>
+    </>
+  );
+};
+
+export default MainTodo;
