8dfdd9bb6a00fe368e9c7b737dbb725efb9e2551
Sayan <sayand.capitalnumbers.com>
2023-08-27 12:34:36 +0530

    new upload

--- src/pages/todoApp/MainTodo.js
+++ src/pages/todoApp/MainTodo.js
@@ -3,8 +3,41 @@ import React from 'react';
 const MainTodo = () => {
   return (
     <>
-      <div>gjhghj</div>
-      <div>gjhghj</div>
+      <div className="container mt-4">
+      <h2>Contact Us</h2>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value=""
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value=""
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">Message</label>
+          <textarea
+            className="form-control"
+            id="message"
+            name="message"
+            value=""
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
     </>
   );
 };
