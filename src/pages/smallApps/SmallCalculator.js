65e691d10af1ba5f6597ce143cddc8d8854af7ee
Sayan <sayand.capitalnumbers.com>
2023-09-08 19:02:54 +0530

    calculater

--- src/pages/smallApps/SmallCalculator.js
+++ src/pages/smallApps/SmallCalculator.js
@@ -10,6 +10,7 @@ const SmallCalculator = () => {
   const plusClick = () => {
     const plusData = parseInt(firstInput) + parseInt(secondInput);
     // console.log('plusData=>', plusData);
+    //parseInt use to convert string to number
     setResult(plusData);
     setTimeout(() => {
       setFirstInput('');
@@ -17,6 +18,39 @@ const SmallCalculator = () => {
       setResult('');
     }, 3000);
   };
+
+  const minusClick =()=> {
+     const minusData = parseInt(firstInput) - parseInt(secondInput);
+     console.log('minusData=>', minusData);
+     setResult(minusData);
+     setTimeout(() => {
+      setFirstInput('');
+      setSecondInput('');
+      setResult('');
+    }, 3000);
+  };
+
+  const multipleClick =()=> {
+    const multipleData = parseInt(firstInput) * parseInt(secondInput);
+    console.log('multipleData=>', multipleData);
+    setResult(multipleData);
+    setTimeout(() => {
+     setFirstInput('');
+     setSecondInput('');
+     setResult('');
+   }, 3000);
+ }; 
+
+ const divisionClick =()=> {
+  const divisionData = parseInt(firstInput) / parseInt(secondInput);
+  console.log('divisionData=>', divisionData);
+  setResult(divisionData);
+  setTimeout(() => {
+   setFirstInput('');
+   setSecondInput('');
+   setResult('');
+ }, 3000);
+}; 
 
   const resetField = () => {
     setFirstInput('');
@@ -64,16 +98,19 @@ const SmallCalculator = () => {
             <span style={{ fontSize: 25 }}>+</span>
           </button>
 
-          <button className="m-4 btn btn-primary" style={{ width: 56 }}>
+          <button className="m-4 btn btn-primary" style={{ width: 56 }}
+          onClick={() => minusClick()}>
             <span style={{ fontSize: 25 }}> -</span>
           </button>
 
           <button className="m-4 btn btn-warning" style={{ width: 56 }}>
-            <span style={{ fontSize: 25 }}>x</span>
+            <span style={{ fontSize: 25 }}
+            onClick={() => multipleClick()}>x</span>
           </button>
 
           <button className="m-4 btn btn-info" style={{ width: 56 }}>
-            <span style={{ fontSize: 25 }}> ÷</span>
+            <span style={{ fontSize: 25 }}
+            onClick={() => divisionClick()}> ÷</span>
           </button>
           <button
             className="m-4 btn btn-danger"
