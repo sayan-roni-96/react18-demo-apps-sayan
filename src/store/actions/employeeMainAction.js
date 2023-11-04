9df8115c1c24fe2ee18aa2481965c55ef4c75841
prabhat-cn <prabhat@capitalnumbers.com>
2023-11-04 18:38:08 +0530

    updated code

--- src/store/actions/employeeMainAction.js
+++ src/store/actions/employeeMainAction.js
@@ -4,9 +4,9 @@ import { RootApi } from '../../RootApi';
 export const getAllMainEmployees = createAsyncThunk(
   'employee/get',
   async () => {
-    // const response = await axios.get(`${baseUrl}/employees`);
+    // const response = await axios.get(`${rootBaseUrl}/employees`);
     const response = await RootApi.get(`/employees`);
-    // console.log('response-a', response);
-    return response.data.reverse();
+    console.log('response-get', response);
+    return response.data;
   }
 );
