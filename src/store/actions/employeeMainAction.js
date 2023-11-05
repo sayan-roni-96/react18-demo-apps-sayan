8ff44836fc3a72b158b57ce2045ddb10a61073cd
prabhat-cn <prabhat@capitalnumbers.com>
2023-11-05 16:12:03 +0530

    🚑

--- src/store/actions/employeeMainAction.js
+++ src/store/actions/employeeMainAction.js
@@ -5,7 +5,7 @@ export const getAllMainEmployees = createAsyncThunk(
   'employee/get',
   async () => {
     // const response = await axios.get(`${rootBaseUrl}/employees`);
-    const response = await RootApi.get(`/employees`);
+    const response = await RootApi.get(`/employee`);
     console.log('response-get', response);
     return response.data;
   }
